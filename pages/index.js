e18206a066a75ca818289c0ea0b4991a54cc79a2
PatoGordo <59514690+PatoGordo@users.noreply.github.com>
2021-01-26 12:05:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ export default function Main(){
   return(
     <div className="ApiDocs">
       <h2>Api Docs</h2>
+      <p style={{color: "#FF7043"}}><strong>Coming soon</strong></p>
       <p><strong>End Points</strong></p>
       <ul>
-        <li>All: <a href="https://nextapi.patogordo.repl.co/api/v1/all" >Click Here!</a></li>
-        <li>Hex: <a href="https://nextapi.patogordo.repl.co/api/v1/hex" >Click Here!</a></li>
-        <li>RGB: <a href="https://nextapi.patogordo.repl.co/api/v1/rgb" >Click Here!</a></li>
-        <li>RGBA: <a href="https://nextapi.patogordo.repl.co/api/v1/rgba" >Click Here!</a></li>
-        <li>Hexa: <a href="https://nextapi.patogordo.repl.co/api/v1/hexa" >Click Here!</a></li>
+        <li>All: <a href="https://random-color-api.vercel.app/api/v1/all" >Click Here!</a></li>
+        <li>Hex: <a href="https://random-color-api.vercel.app/api/v1/hex" >Click Here!</a></li>
+        <li>RGB: <a href="https://random-color-api.vercel.app/api/v1/rgb" >Click Here!</a></li>
+        <li>RGBA: <a href="https://random-color-api.vercel.app/api/v1/rgba" >Click Here!</a></li>
+        <li>Hexa: <a href="https://random-color-api.vercel.app/api/v1/hexa" >Click Here!</a></li>
       </ul>
     </div>
   )
